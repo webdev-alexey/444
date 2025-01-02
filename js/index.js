2d0085da9190024179ab1919daaa373914cd9665
alexey <alexey.anasov@yandex.by>
2025-01-03 00:14:45 +0300

    fix burger-меню

--- js/index.js
+++ js/index.js
@@ -1,8 +1,8 @@
 import slideMenu from "./menu.js";
 
-slideMenu(
-  ".header__burger-btn",
-  ".navigation",
-  "navigation_active",
-  ".navigation__link, .navigation__close"
-);
+slideMenu({
+  openBtn: ".header__burger-btn",
+  menu: ".navigation",
+  classActiveMenu: "navigation_active",
+  closeTrigger: ".navigation__link, .navigation__close",
+});
--- js/menu.js
+++ js/menu.js
@@ -9,9 +9,16 @@ const closeMenu = (nav, active) => {
 const slideMenu = ({ openBtn, menu, classActiveMenu, closeTrigger }) => {
   const burgerBtn = document.querySelector(openBtn);
   const navigation = document.querySelector(menu);
+  const navigationClose = document.querySelectorAll(closeTrigger);
 
   burgerBtn.addEventListener("click", () => {
     openMenu(navigation, classActiveMenu);
+  });
+
+  navigationClose.forEach((item) => {
+    item.addEventListener("click", () => {
+      closeMenu(navigation, classActiveMenu);
+    });
   });
 };
 
